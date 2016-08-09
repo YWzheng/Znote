f1768d4bd29f9254cb7c0b083dac0b56ac01b13f
zhengyongwei <zhengyongwei2015@gmail.com>
2016-08-09 21:44:07 +0800

    2016-8-9 21:43:21 git init

---
+++ app/src/main/java/com/ywzheng/znote/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ywzheng.znote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The project was established in 2016 on August 9, mainly to collect all kinds of UI effect and code
+ *
+ * 2016-8-9
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
