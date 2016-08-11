7bf6897c160cb6b2dbf95dea1b539f18bf6f337c
zhengyongwei <zhengyongwei2015@gmail.com>
2016-08-11 17:59:51 +0800

    2016-8-11 17:59:45 add

--- app/src/main/java/com/ywzheng/znote/MainActivity.java
+++ app/src/main/java/com/ywzheng/znote/MainActivity.java
@@ -1,14 +1,14 @@
 package com.ywzheng.znote;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 /**
  * The project was established in 2016 on August 9, mainly to collect all kinds of UI effect and code
- *
+ * <p>
  * 2016-8-9
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
