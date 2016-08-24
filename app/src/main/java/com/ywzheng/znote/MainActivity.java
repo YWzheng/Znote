af2a58de7eee3ece6419aa54bfcc2781ec4f8e41
zhengyongwei <zhengyongwei2015@gmail.com>
2016-08-24 10:49:15 +0800

    2016-8-24 10:49:09

--- app/src/main/java/com/ywzheng/znote/MainActivity.java
+++ app/src/main/java/com/ywzheng/znote/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_view_12);
+        setContentView(R.layout.activity_main);
 
     }
 
