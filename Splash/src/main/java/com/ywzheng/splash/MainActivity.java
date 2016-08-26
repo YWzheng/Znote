0a5c07ff534a5a5889fb953013f65b61ed8b06d0
zhengyongwei <zhengyongwei2015@gmail.com>
2016-08-26 15:49:01 +0800

    2016-8-26 15:48:51

--- Splash/src/main/java/com/ywzheng/splash/MainActivity.java
+++ Splash/src/main/java/com/ywzheng/splash/MainActivity.java
@@ -2,10 +2,11 @@ package com.ywzheng.splash;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,33 +14,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ywzheng.splash.adpter.FragmentAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.tabs)
+    TabLayout mTabs;
+    @BindView(R.id.viewpager)
+    ViewPager mViewPager;
+    @BindView(R.id.nav_view)
+    NavigationView mNavView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setSupportActionBar(mToolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavView.setNavigationItemSelectedListener(this);
+        FragmentAdapter mAdapter = new FragmentAdapter(getSupportFragmentManager(),getResources().getStringArray(R.array.group_name));
+        mViewPager.setAdapter(mAdapter);
+        mTabs.setupWithViewPager(mViewPager);
     }
 
     @Override
@@ -97,5 +109,11 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @OnClick(R.id.fab)
+    public void onClick() {
+        Snackbar.make(mFab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
---
+++ Splash/src/main/java/com/ywzheng/splash/adpter/ViewPagerAdapter.java
@@ -0,0 +1,76 @@
+package com.ywzheng.splash.adpter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yongwei on 2016/8/26.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+
+    private ArrayList<View> mList;
+
+    /**
+     * @param context
+     */
+    public ViewPagerAdapter(Context context) {
+        mContext = context;
+        mList = new ArrayList<View>();
+    }
+
+    /**
+     * @param item
+     */
+    public void add(View item) {
+        mList.add(item);
+    }
+
+    /**
+     * @param container
+     * @param position
+     * @return
+     */
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View item = mList.get(position);
+        container.addView(item);
+        return item;
+
+    }
+
+    /**
+     * @param container
+     * @param position
+     * @param object
+     */
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    /**
+     * @return　size
+     */
+    @Override
+    public int getCount() {
+        return mList == null ? 0 : mList.size();
+    }
+
+    /**
+     * @param view
+     * @param object
+     * @return true
+     */
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == (View) object;
+    }
+}
