d765a92a70d13fe8dbc6735c1737977a03a57d22
zhengyongwei <zhengyongwei2015@gmail.com>
2016-08-30 16:36:46 +0800

    2016-8-30 16:36:38

--- app/src/main/java/com/ywzheng/znote/Znote.java
+++ app/src/main/java/com/ywzheng/znote/Znote.java
@@ -1,7 +1,11 @@
 package com.ywzheng.znote;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.widget.TextView;
@@ -28,5 +32,25 @@ public class Znote extends AppCompatActivity {
         //代码设置颜色
         TextView mTextView = new TextView(this);
         mTextView.setBackgroundColor(0xff0000bb);
+
+        //AlertDialog 重点在于&#8226;这个符号的使用，列表项
+        new AlertDialog.Builder(this)
+                .setTitle("AppCompat Extension Library")
+                .setMessage("A library that builds on the official AppCompat Design Library and provides additional common components:\\n &#8226; AccountHeaderView\\n &#8226; FloatingActionMenu\\n &#8226;\n" +
+                        "        CircleImageView\\n &#8226; FlexibleToolbarLayout\\n &#8226; Delightful Detail Drawables\\n &#8226; TypefaceCompat")
+                .setNeutralButton("View on Github", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String url = "https://github.com/TR4Android/AppCompat-Extension-Library";
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                    }
+                })
+                .setNegativeButton("Dismiss", null)
+                .show();
+
+
+
     }
 }
