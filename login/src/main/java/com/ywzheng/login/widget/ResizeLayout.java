4519ba0e58e193a74db8b48b8984bacd719e0348
zhengyongwei <zhengyongwei2015@gmail.com>
2016-09-02 10:23:00 +0800

    2016-9-2 10:22:44

---
+++ login/src/main/java/com/ywzheng/login/widget/ResizeLayout.java
@@ -0,0 +1,44 @@
+package com.ywzheng.login.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * 监听布局的高度变化
+ *
+ */
+
+public class ResizeLayout extends RelativeLayout {
+    private OnResizeListener mOnResizeListener;
+
+    public interface OnResizeListener {
+        void OnResize(int w, int h, int oldw, int oldh);
+    }
+
+
+    public void setOnSizeChangedListener(OnResizeListener listener) {
+        this.mOnResizeListener = listener;
+    }
+
+    public ResizeLayout(Context context) {
+        super(context);
+    }
+
+    public ResizeLayout(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public ResizeLayout(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+    }
+
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if (mOnResizeListener != null) {
+            mOnResizeListener.OnResize(w, h, oldw, oldh);
+        }
+    }
+}
