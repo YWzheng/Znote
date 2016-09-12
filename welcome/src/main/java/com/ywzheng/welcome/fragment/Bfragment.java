44761e5d804c1495a39b7a94243e1dbb816fc0ea
zhengyongwei <zhengyongwei2015@gmail.com>
2016-09-12 15:30:43 +0800

    2016-9-12 15:30:34

--- welcome/src/main/java/com/ywzheng/welcome/fragment/Bfragment.java
+++ welcome/src/main/java/com/ywzheng/welcome/fragment/Bfragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ywzheng.welcome.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by yongwei on 2016/8/31.
@@ -16,6 +20,8 @@ import com.ywzheng.welcome.R;
 public class Bfragment extends Fragment {
 
     private static Bfragment bFragment;
+    @BindView(R.id.splash)
+    ImageView mSplash;
 
     public static Bfragment newInstance() {
         if (bFragment != null) {
@@ -32,6 +38,9 @@ public class Bfragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.splash_view_b, container, false);
+        View view = inflater.inflate(R.layout.splash_view_b, container, false);
+        ButterKnife.bind(this, view);
+        mSplash.animate().scaleX(0.12f).scaleY(0.12f).setDuration(2000).setStartDelay(100).start();
+        return view;
     }
 }
--- welcome/src/main/java/com/ywzheng/welcome/fragment/Ffragment.java
+++ welcome/src/main/java/com/ywzheng/welcome/fragment/Ffragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ywzheng.welcome.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by yongwei on 2016/8/31.
@@ -16,6 +20,8 @@ import com.ywzheng.welcome.R;
 public class Ffragment extends Fragment {
 
     private static Ffragment bFragment;
+    @BindView(R.id.splash_image)
+    ImageView mSplashImage;
 
     public static Ffragment newInstance() {
         if (bFragment != null) {
@@ -32,6 +38,10 @@ public class Ffragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.splash_view_f, container, false);
+        View view = inflater.inflate(R.layout.splash_view_f, container, false);
+        ButterKnife.bind(this, view);
+
+        mSplashImage.animate().scaleX(1.12f).scaleY(1.12f).setDuration(2000).setStartDelay(100).start();
+        return view;
     }
 }
