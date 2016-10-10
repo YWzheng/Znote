a729f9ecf33dea62dcf1e88995bae643f437f89c
zhengyongwei <zhengyongwei2015@gmail.com>
2016-10-10 14:59:08 +0800

    2016-10-10 14:58:52

--- app/src/main/java/com/ywzheng/znote/ui/base/BaseActivity.java
+++ app/src/main/java/com/ywzheng/znote/ui/base/BaseActivity.java
@@ -3,15 +3,49 @@ package com.ywzheng.znote.ui.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.ywzheng.znote.R;
 
 /**
  * Created by yongwei on 2016/8/23.
  */
-public class BaseActivity extends AppCompatActivity{
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(setInflateId());
+        init();
+        initAcition();
+    }
 
+    //设置布局id
+    public abstract int setInflateId();
+
+    //视图，组件,数据的初始化
+    public abstract void init();
+
+    //事件监听
+    public void initAcition(){}
+
+    //Activity设置带返回按钮的Toolbar
+    public void setupBackToolbar(String title){
+        setupBackToolbar(R.id.toolbar,title);
+    }
+
+    public void setupBackToolbar(int toolbarId,String title){
+        Toolbar mToolbar = (Toolbar) findViewById(toolbarId);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //显示小箭头
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        setTitle(title);
     }
 }
